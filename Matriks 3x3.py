abf60a37527041ecf48feb7a11ce45ddc25a9edf
Nurultazqya <ntazqyah@gmail.com>
2024-10-07 01:12:50 +0800

    Update Matriks 3x3.py

--- Matriks 3x3.py
+++ Matriks 3x3.py
@@ -4,11 +4,11 @@ def multiply_matrices(A, B):
 
     result = [[0 for_in range(len(B[0]))]
 
- for_in range(len(A))]
-    for i in range(len(A)):
-        for j in range(len(B[0])):
-            for k in range(len(B)):
-                result[i][j] += A[i][k]*B[k][j]
+    for_in range(len(A))]
+        for i in range(len(A)):
+            for j in range(len(B[0])):
+                for k in range(len(B)):
+                    result[i][j] += A[i][k]*B[k][j]
 
     retusrn result 
   
