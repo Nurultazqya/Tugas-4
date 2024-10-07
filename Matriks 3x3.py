9c3b638ed4a7089e80aec1d4d3f253dcabea999a
Nurultazqya <ntazqyah@gmail.com>
2024-10-07 12:09:27 +0800

    Update Matriks 3x3.py

--- Matriks 3x3.py
+++ Matriks 3x3.py
@@ -1,18 +1,17 @@
 def multiply_matrices(A, B):
     if len(A[0]) != len(B):
-      return "matriks tidak dapat dikalikan"
+       return "Matriks tidak dapat dikalikan"
 
-    result = [[0 for_in range(len(B[0]))]
+    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))] 
 
-    for_in range(len(A))]
-        for i in range(len(A)):
-            for j in range(len(B[0])):
-                for k in range(len(B)):
-                    result[i][j] += A[i][k]*B[k][j]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
 
-    retusrn result 
-  
+    return result 
+
 # contoh penggunaan
-A = [[1,2,2],[4,5,6],[7,8,9]]
-B = [[9,8,7],[6,5,4],[3,2,1]]
+A = [[1,2,4],[3,5,6],[7,2,9]]
+B = [[3,8,5],[3,5,4],[3,6,1]]
 print(multiply_matrices(A, B))
